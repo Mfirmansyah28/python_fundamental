4d67b9076779a3ba86bdfbf5cbdf95257ebefdc3
M.Firmansyah <muhammadfirmansyah401@gmail.com>
2026-08-16 18:54:23 +0700

    add input.py & update variabel.py

---
+++ input.py
@@ -0,0 +1,9 @@
+name = input ('Masukkan Nama Anda: ')
+print (name)
+
+"""
+Output:
+Masukkan Nama Anda: M.Firmansyah
+M.Firmansyah
+"""
+
--- variabel.py
+++ variabel.py
@@ -6,3 +6,11 @@ result adalah hasil pengurangan dari variabel addition dikurangi 1
 additional = 2+2
 result = additional - 1
 print(result)
+
+""""
+TODO:
+- Buatlah sebuah variabel bernama greeting.
+- Assign teks "Saya mulai belajar Python!" pada variabel tersebut. 
+"""
+
+greeting = "Saya mulai belajar Python!"
