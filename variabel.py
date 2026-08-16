3d37046553ae748024052a987add201869cbb302
M.Firmansyah <muhammadfirmansyah401@gmail.com>
2026-08-16 18:29:49 +0700

    add variabel.py

---
+++ variabel.py
@@ -0,0 +1,8 @@
+"""
+addition adalah variabel yang bernilai ekspresi 2+2,
+result adalah hasil pengurangan dari variabel addition dikurangi 1
+"""
+
+additional = 2+2
+result = additional - 1
+print(result)
