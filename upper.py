81d22f792c64a739124575f213f9206c4ca28cf1
M.Firmansyah <muhammadfirmansyah401@gmail.com>
2026-08-20 19:41:32 +0700

    add file upper.py

---
+++ upper.py
@@ -0,0 +1,19 @@
+"""
+Mengubah Huruf Besar/Kecil
+Dalam kategori ini terdapat beberapa metode yang dapat 
+digunakan untuk mengubah string menjadi huruf kapital 
+(UPPERCASE) atau huruf kecil (lowercase).
+Kedua metode ini, baik upper() maupun lower() adalah metode bawaan dari string Python. Perlu diingat bahwa jika terdapat 
+karakter bukan huruf (seperti simbol atau angka) 
+yang tidak memiliki opsi kapital sehingga karakter 
+tersebut tidak mengalami perubahan.
+"""
+
+kata = 'dicoding'
+kata = kata.upper()
+print(kata) 
+
+"""
+Output:
+DICODING
+"""
