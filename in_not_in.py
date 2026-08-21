4673c33e084bb22b7aff9e63cb604a6b550b09ed
M.Firmansyah <muhammadfirmansyah401@gmail.com>
2026-08-21 21:30:08 +0700

    add file in & not in

---
+++ in_not_in.py
@@ -0,0 +1,13 @@
+kalimat = "Belajar Python di Dicoding sangat Menyenangkan"
+print('Dicoding' in kalimat)
+print('tidak' in kalimat)
+print('Dicoding' not in kalimat)
+print('tidak' not in kalimat)
+
+"""
+Output:
+True
+False
+False
+True
+"""
