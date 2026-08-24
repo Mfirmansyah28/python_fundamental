26856ce1593afd7c9cee8224e2b98568669d472b
M.Firmansyah <muhammadfirmansyah401@gmail.com>
2026-08-24 16:49:43 +0700

    add file aksi_Sekuensial.py

---
+++ aksi_sekuensial.py
@@ -0,0 +1,20 @@
+print("Selamat datang dalam program Python!\n")
+print("Silahkan masukkan data diri anda.")
+nama = input("Masukkan nama anda: ")
+tahun_lahir = input("Masukkan tahun lahir anda: ")
+umur = 2023 - int (tahun_lahir)
+
+print(f"Selamat datang {nama} dalam program Python, per 2023 umur anda adaalh {umur} tahun.\n")
+print("Terima kasih telah menggunakan program Python!")
+
+"""
+Output:
+Selamat datang dalam program Python!
+ 
+Silakan masukkan data diri Anda:
+Masukkan nama Anda: Evans
+Masukkan tahun lahir Anda: 2005
+Selamat datang Evans dalam program Python, per 2023 umur Anda adalah 18 tahun.
+ 
+Terima kasih telah menggunakan program Python!
+"""
