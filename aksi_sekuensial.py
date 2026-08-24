578e1dc1f3f16d5fd171d88bbdc83bca98b61bc4
M.Firmansyah <muhammadfirmansyah401@gmail.com>
2026-08-24 16:55:35 +0700

    fix variabel tahun lahir

--- aksi_sekuensial.py
+++ aksi_sekuensial.py
@@ -2,9 +2,9 @@ print("Selamat datang dalam program Python!\n")
 print("Silahkan masukkan data diri anda.")
 nama = input("Masukkan nama anda: ")
 tahun_lahir = input("Masukkan tahun lahir anda: ")
-umur = 2023 - int (tahun_lahir)
+umur = 2026 - int (tahun_lahir)
 
-print(f"Selamat datang {nama} dalam program Python, per 2023 umur anda adaalh {umur} tahun.\n")
+print(f"Selamat datang {nama} dalam program Python, per 2026 umur anda adaalh {umur} tahun.\n")
 print("Terima kasih telah menggunakan program Python!")
 
 """
@@ -14,7 +14,7 @@ Selamat datang dalam program Python!
 Silakan masukkan data diri Anda:
 Masukkan nama Anda: Evans
 Masukkan tahun lahir Anda: 2005
-Selamat datang Evans dalam program Python, per 2023 umur Anda adalah 18 tahun.
+Selamat datang Evans dalam program Python, per 2026 umur Anda adalah 18 tahun.
  
 Terima kasih telah menggunakan program Python!
 """
