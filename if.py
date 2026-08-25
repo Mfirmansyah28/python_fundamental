ddae6ca7b18cceed025a82c8c4e1479ef12d1ecb
M.Firmansyah <muhammadfirmansyah401@gmail.com>
2026-08-25 19:01:28 +0700

    add file percabangan & if

---
+++ if.py
@@ -0,0 +1,9 @@
+score = 100
+
+if score == 100:
+    print("Nilai Anda Sempurna")
+
+"""
+Output:
+Nilai Anda Sempurna!
+"""
---
+++ percabangan.py
@@ -0,0 +1,12 @@
+ketersediaan = "Daging ayam"
+
+if ketersediaan == "Daging ayam":
+    print("Ibu membeli dan memasak ayam")
+
+else:
+    print("Ibu membeli dan memasak tempe")
+
+"""
+Output:
+Ibu membeli dan memasak ayam
+"""
